200416d2b4e26a4e0930ea1f70cd24c60e1f3dd6
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2024-05-08 00:32:53 +0800

    rebasing

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,5 +1,19 @@
 import React, { FC } from "react";
+import LoginButton from "../components/auth0/LoginButton";
+import LogoutButton from "../components/auth0/LogoutButton";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export const Events: FC = () => {
-  return <h1 className="bg-gray-600 w-full text-white">Events</h1>;
+  return (
+    <h1 className="bg-gray-600 w-full text-white">
+      Events
+      <Auth0Provider
+        domain="dev-75ifw6lzojcgig6l.us.auth0.com"
+        clientId="REAZfqYGbS9C1CyIACzjDbNbtUSPEnzo"
+      >
+        <LoginButton />
+        <LogoutButton />
+      </Auth0Provider>
+    </h1>
+  );
 };
